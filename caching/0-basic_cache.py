ff824cdf0ae447234a9bda6361f69519d549861f
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2024-05-27 12:53:44 +0300

    caching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,20 +1,48 @@
-#!/usr/bin/env python3
-""" Async Generator """
-import asyncio
-import random
-from typing import Generator
+#!/usr/bin/python3
+"""
+    BaseCache module
+"""
+
+from base_caching import BaseCaching
 
 
-async def async_generator() -> Generator[float, None, None]:
+class BasicCache(BaseCaching):
+    """ BasicCache define a intro to use cache
+
+      To use:
+      >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+      >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      >>> print(my_cache.get("A"))
+      Hello
     """
-        Generate numbers
 
-        Args:
-            void
+    def put(self, key, item):
+        """
+            modify cache data
 
-        Return:
-            float time random
-    """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+            Args:
+                key: of the dict
+                item: value of the key
+        """
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            modify cache data
+
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
+        """
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,24 +1,78 @@
-#!/usr/bin/env python3
-""" Measure times """
-import asyncio
-import time
+#!/usr/bin/python3
+"""
+    BaseCache module
+"""
 
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+from base_caching import BaseCaching
 
 
-async def measure_runtime() -> float:
+class LIFOCache(BaseCaching):
+    """ LIFOCache define a FIFO algorithm to use cache
+
+      To use:
+      >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+      >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      >>> print(my_cache.get("A"))
+      Hello
+
+      Ex:
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Holberton", D: "School"}
+      >>> my_cache.put("C", "Street")
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", D: "School",  C: "Street"}
+
+      >>> my_cache.put("F", "COD")
+      DISCARD: C
+      >>> print(self.cache_data)
+      {F: "COD", B: "World", D: "School", F, "COD"}
     """
-        measure time and execute in paralallel
 
-        Args:
-            void
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
 
-        Return:
-            float random numbers
-    """
-    first_time = time.perf_counter()
-    tasks = [async_comprehension() for _ in range(4)]
-    await asyncio.gather(*tasks)
-    elapsed = time.perf_counter()
+    def put(self, key, item):
+        """
+            modify cache data
 
-    return (elapsed - first_time)
+            Args:
+                key: of the dict
+                item: value of the key
+        """
+        if key or item is not None:
+            valuecache = self.get(key)
+            # Make a new
+            if valuecache is None:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    keydel = list(self.cache_data.keys())
+                    lenlast = len(keydel) - 1
+                    del self.cache_data[keydel[lenlast]]
+                    print("DISCARD: {}".format(keydel[lenlast]))
+            # If it's None this del the key and after update the same key
+            # If it's wrong fix eliminate and ask
+            else:
+                del self.cache_data[key]
+            # Modify value
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            modify cache data
+
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
+        """
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
