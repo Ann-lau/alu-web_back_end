2d2f7305f74302fa8878da326cc1c2ea7f1b40ae
Ann Lau <101356391+Annedumo@users.noreply.github.com>
2024-07-06 15:50:20 +0300

    parametize templates

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -16,15 +16,18 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object('4-app.Config')
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale() -> str:
     ''' Determine best match with supported languages '''
+    # Check if locale is specified in URL parameter
     locale = request.args.get('locale')
     if locale and locale in app.config['LANGUAGES']:
         return locale
+
+    # Use accept_languages.best_match as fallback
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
